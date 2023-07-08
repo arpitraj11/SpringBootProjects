e88cd48192ca6b7ca2c5fb2dead960a23eaa6f46
Arpit <guptarpit11@gmail.com>
2023-07-08 17:08:33 +0530

    Employee-Address ONEtoONEMapping

---
+++ Employee-Address/src/main/java/com/geekster/EmployeeAddress/model/Employee.java
@@ -0,0 +1,22 @@
+package com.geekster.EmployeeAddress.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String firstName;
+    private String lastName;
+
+    @OneToOne
+    @JoinColumn(name = "fk_address")
+    Address address;
+}
